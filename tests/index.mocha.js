d20f0dc2591fb50fb53b2c7be3b5bc9ef6026a5a
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-07-13 18:05:57 +0200

    Switching to streamtest

--- tests/index.mocha.js
+++ tests/index.mocha.js
@@ -1,73 +1,103 @@
 var fs = require('fs');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var es = require('event-stream');
+var streamtest = require('streamtest');
 var mdvars = require('../src/index');
 var assert = require('assert');
 
 
 describe('gulp-mdvars', function() {
 
-  describe('with null contents', function() {
+  // Iterating through versions
+  streamtest.versions.forEach(function(version) {
 
-    it('should let null files pass through', function(done) {
+    describe('for ' + version + ' streams', function() {
 
-      var s = mdvars();
-      var n = 0;
-      s.pipe(es.through(function(file) {
-          assert.equal(file.path,'bibabelula.md');
-          assert.equal(file.contents, null);
-          n++;
-        }, function() {
-          assert.equal(n,1);
-          done();
-        }));
-      s.write(new gutil.File({
-        path: 'bibabelula.md',
-        contents: null
-      }));
-      s.end();
+      describe('with null contents', function() {
 
-    });
+        it('should let null files pass through', function(done) {
 
-  });
+          var stream = mdvars();
+          stream.pipe(streamtest[version].toObjects(function(err, files) {
+            if(err) {
+              return done(err);
+            }
+            assert.equal(files.length, 1);
+            assert.equal(files[0].path, 'bibabelula.md');
+            assert.equal(files[0].contents, null);
+            done();
+          }));
+          stream.write(new gutil.File({
+            path: 'bibabelula.md',
+            contents: null
+          }));
+          stream.end();
 
-  describe('in stream mode', function() {
+        });
 
-    it('should work with a valid file', function(done) {
-      gulp.src('tests/fixtures/simple.meta.md', {buffer: false})
-        .pipe(mdvars())
-        .pipe(es.map(function(file){
-          file.contents.pipe(es.wait(function(err, data) {
-            assert(file.metas);
-            assert.equal(file.metas.title, 'A markdown file');
-            assert.equal(file.metas.description, 'This is a simple markdown file');
+      });
+
+      describe('in stream mode', function() {
+
+        it('should work with a valid file', function(done) {
+
+          var stream = mdvars();
+          stream.pipe(streamtest[version].toObjects(function(err, files) {
+            if(err) {
+              return done(err);
+            }
+            assert.equal(files.length, 1);
+            assert.equal(files[0].path, 'tests/fixtures/simple.meta.md');
+            files[0].contents.pipe(streamtest[version].toText(function(err, text) {
+              assert(files[0].metas);
+              assert.equal(files[0].metas.title, 'A markdown file');
+              assert.equal(files[0].metas.description, 'This is a simple markdown file');
+              assert.equal(
+                text,
+                fs.readFileSync('tests/fixtures/simple.md', 'utf-8')
+              );
+              done();
+            }));
+          }));
+          stream.write(new gutil.File({
+            path: 'tests/fixtures/simple.meta.md',
+            contents: fs.createReadStream('tests/fixtures/simple.meta.md')
+          }));
+          stream.end();
+        });
+
+      });
+
+      describe('in buffer mode', function() {
+
+        it('should work with a valid file', function(done) {
+
+          var stream = mdvars();
+          stream.pipe(streamtest[version].toObjects(function(err, files) {
+            if(err) {
+              return done(err);
+            }
+            assert.equal(files.length, 1);
+            assert.equal(files[0].path, 'tests/fixtures/simple.meta.md');
+            assert(files[0].metas);
+            assert.equal(files[0].metas.title, 'A markdown file');
+            assert.equal(files[0].metas.description, 'This is a simple markdown file');
             assert.equal(
-              data,
-              fs.readFileSync('tests/fixtures/simple.md', 'utf8')
+              files[0].contents.toString('utf-8'),
+              fs.readFileSync('tests/fixtures/simple.md', 'utf-8')
             );
             done();
           }));
-        }));
-    });
+          stream.write(new gutil.File({
+            path: 'tests/fixtures/simple.meta.md',
+            contents: fs.readFileSync('tests/fixtures/simple.meta.md')
+          }));
+          stream.end();
 
-  });
+        });
 
-  describe('in buffer mode', function() {
+      });
 
-    it('should work with a valid file', function(done) {
-      gulp.src('tests/fixtures/simple.meta.md', {buffer: true})
-        .pipe(mdvars())
-        .pipe(es.map(function(file) {
-          assert(file.metas);
-          assert.equal(file.metas.title, 'A markdown file');
-          assert.equal(file.metas.description, 'This is a simple markdown file');
-          assert.equal(
-            file.contents.toString('utf-8'),
-            fs.readFileSync('tests/fixtures/simple.md', 'utf-8')
-          );
-          done();
-        }));
     });
 
   });
